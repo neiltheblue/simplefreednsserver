ce2d1e8df6c3329a94a7af1e7e8e40be92f8dc6b
Neil Blue <neil.the.blue+gh1@gmail.com>
2016-10-19 12:19:27 +0100

    Update freeupdate.sh
    moved output file

--- freeupdate.sh
+++ freeupdate.sh
@@ -2,7 +2,8 @@
 
 while true
 do
-   wget -q -O - http://freedns.afraid.org/dynamic/update.php?${API_KEY} > /freedns.log
+   rm -f /freedns.log
+   wget -q -O /freedns.log http://freedns.afraid.org/dynamic/update.php?${API_KEY}
    
    # sleep for 3 hours 60*60*3
    sleep 10800
