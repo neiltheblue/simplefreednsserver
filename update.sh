a1eabf205f785e9fd53106364cbf327603134207
Neil Blue <neil.the.blue+gh1@gmail.com>
2016-10-19 10:49:13 +0100

    Update update.sh
    Added quite options

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 while true
 do
    echo "Update dns.."   
-   wget http://freedns.afraid.org/dynamic/update.php?${API_KEY}
+   wget -q -O /dev/null http://freedns.afraid.org/dynamic/update.php?${API_KEY}
    
    # sleep for 3 hours 60*60*3
    sleep 10800
