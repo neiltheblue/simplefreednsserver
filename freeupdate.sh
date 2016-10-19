3438cf24d0684bd72cacfc5969fb88c3a806891c
Neil Blue <neil.the.blue+gh1@gmail.com>
2016-10-19 12:23:18 +0100

    Update freeupdate.sh
    removed file path

--- freeupdate.sh
+++ freeupdate.sh
@@ -2,8 +2,8 @@
 
 while true
 do
-   rm -f /freedns.log
-   wget -q -O /freedns.log http://freedns.afraid.org/dynamic/update.php?${API_KEY}
+   rm -f freedns.log
+   wget -q -O freedns.log http://freedns.afraid.org/dynamic/update.php?${API_KEY}
    
    # sleep for 3 hours 60*60*3
    sleep 10800
